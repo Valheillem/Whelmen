8c0767a0445fc7b2b00fa506c67bd7c2520fc158
Valheillem <agar.828@hotmail.com>
2026-05-22 17:45:38 -0700

    forced browser cache busting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { Start } from './scenes/Start.js?v=1.0.4';
-import { Lobby } from './scenes/Lobby.js?v=1.0.4';
-import { Game } from './scenes/Game.js?v=1.0.4';
+import { Start } from './scenes/Start.js?v=1.0.5';
+import { Lobby } from './scenes/Lobby.js?v=1.0.5';
+import { Game } from './scenes/Game.js?v=1.0.5';
 
 const config = {
     type: Phaser.AUTO,
