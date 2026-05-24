4b671296be7337deb44433b57ac24aeee6d67c25
Valheillem <agar.828@hotmail.com>
2026-05-24 14:58:50 -0700

    made sure spell description doesn't clip into other buttons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { Start } from './scenes/Start.js?v=1.1.2';
-import { Lobby } from './scenes/Lobby.js?v=1.1.2';
-import { Game } from './scenes/Game.js?v=1.1.2';
+import { Start } from './scenes/Start.js?v=1.1.3';
+import { Lobby } from './scenes/Lobby.js?v=1.1.3';
+import { Game } from './scenes/Game.js?v=1.1.3';
 
 const config = {
     type: Phaser.AUTO,
