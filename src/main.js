4bc8e3c6586077d395b00e24176b8dafddb2160c
Valheillem <agar.828@hotmail.com>
2026-07-08 20:38:34 -0700

    Updated main menu UI

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { Start } from './scenes/Start.js?v=1.1.19';
-import { Lobby } from './scenes/Lobby.js?v=1.1.19';
-import { Game } from './scenes/Game.js?v=1.1.19';
+import { Start } from './scenes/Start.js?v=1.1.20';
+import { Lobby } from './scenes/Lobby.js?v=1.1.20';
+import { Game } from './scenes/Game.js?v=1.1.20';
 
 const config = {
     type: Phaser.AUTO,
