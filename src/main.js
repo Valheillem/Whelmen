6d500a3430b85fbdd4a2524faf72256b89fa97b6
Valheillem <agar.828@hotmail.com>
2026-07-09 10:54:51 -0700

    Updated text colors

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { Start } from './scenes/Start.js?v=1.1.24';
-import { Lobby } from './scenes/Lobby.js?v=1.1.24';
-import { Game } from './scenes/Game.js?v=1.1.24';
+import { Start } from './scenes/Start.js?v=1.1.25';
+import { Lobby } from './scenes/Lobby.js?v=1.1.25';
+import { Game } from './scenes/Game.js?v=1.1.25';
 
 const config = {
     type: Phaser.AUTO,
