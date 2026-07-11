5ae99a2788c719e1b465179ccda613bec5493109
Valheillem <agar.828@hotmail.com>
2026-07-11 12:55:35 -0700

    updated icon one more time

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { Start } from './scenes/Start.js?v=1.2.5';
-import { Lobby } from './scenes/Lobby.js?v=1.2.5';
-import { Game } from './scenes/Game.js?v=1.2.5';
+import { Start } from './scenes/Start.js?v=1.2.6';
+import { Lobby } from './scenes/Lobby.js?v=1.2.6';
+import { Game } from './scenes/Game.js?v=1.2.6';
 
 const config = {
     type: Phaser.AUTO,
