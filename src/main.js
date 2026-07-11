64530821334a56f4260cd77ac4cc62b3cf5c38f0
Valheillem <agar.828@hotmail.com>
2026-07-11 14:33:54 -0700

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { Start } from './scenes/Start.js?v=1.2.21';
-import { Lobby } from './scenes/Lobby.js?v=1.2.21';
-import { Game } from './scenes/Game.js?v=1.2.21';
+import { Start } from './scenes/Start.js?v=1.2.22';
+import { Lobby } from './scenes/Lobby.js?v=1.2.22';
+import { Game } from './scenes/Game.js?v=1.2.22';
 
 const config = {
     type: Phaser.AUTO,
