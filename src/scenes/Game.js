64c27155a33cd4efb873e09e6555e4b7a1e1f2a7
Valheillem <agar.828@hotmail.com>
2026-07-19 22:01:31 -0700

    Update Game.js

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -900,7 +900,7 @@ export class Game extends Phaser.Scene {
             
             const pos = this.getPlayerPositionIndex(pid);
             let sx = 0, sy = 0;
-            if (pos === 0) { sx = -70; sy = 175; }
+            if (pos === 0) { sx = -70; sy = 170; }
             else if (pos === 1) { sx = 80; sy = -150; }
             else if (pos === 2) { sx = -200; sy = 35; }
             else if (pos === 3) { sx = 100; sy = -150; }
